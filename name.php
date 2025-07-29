524c5c2a1a7ec481bee2bd4362bfa6ceaed317ca
Nathan Lobo <lobonathan2209@gmail.com>
2025-07-29 14:32:28 +0530

    Initial Commit - Day 1 - 29/07

---
+++ name.php
@@ -0,0 +1,21 @@
+<?php
+print"Nathan";
+echo'<br>';
+echo'<br>';
+echo'<img src="img.jpg" width="300" height="300">';
+echo'<br>';
+echo'<br>';
+echo'<a href="https://www.youtube.com/ElectroProjects">My YouTube Channel</a>';
+echo'<br>';
+echo'<br>';
+echo'<a href="https://www.youtube.com/ElectroProjects"><img src="img.jpg"></a>';
+echo'<br>';
+echo'<br>';
+$name="Nathan Lobo";
+$age=20;
+echo"My Name is $name & My age is $age";
+echo'<br><br>';
+echo"Using For Loop <br>";
+for($i=1;$i<=10;$i++)
+{echo"number is $i<br>";}
+?>
